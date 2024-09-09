a90dee346f3d2af9ae243feb1d95e729b17d9707
m1nhnguy3n <minhdacamst@gmail.com>
2024-09-09 14:08:59 +0700

    feat(user-module) : Refactor get and delete user services

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -84,7 +84,8 @@ export class UserController {
     }
   }
 
-  @Get('a')
+  @Get('teacher')
+  @ApiOkResponse({ description: 'Get all teachers successfully' })
   async findAllTeacher() {
     try {
       const data = await this.userService.findAllTeacher();
@@ -102,6 +103,7 @@ export class UserController {
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'Get user by id successfully' })
   async findOne(@Param('id', new ParseUUIDPipe()) id: UUID) {
     try {
       const data = await this.userService.findOne(id);
@@ -119,6 +121,7 @@ export class UserController {
   }
 
   @Get('teacher/:id')
+  @ApiOkResponse({ description: 'Get teacher by id successfully' })
   async findTeacher(@Param('id', new ParseUUIDPipe()) id: UUID) {
     try {
       const data = await this.userService.findOneTeacher(id);
@@ -135,6 +138,43 @@ export class UserController {
     }
   }
 
+  @Post('student')
+  @ApiCreatedResponse({ description: 'The student has been successfully created.' })
+  async createStudent(@Body() createStudentDto: CreateStudentDto) {
+    try {
+      await this.userService.createStudent(createStudentDto, createStudentDto.parentNumber);
+
+      return {
+        success: true,
+        message: 'Student Created Successfully'
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message
+      };
+    }
+  }
+
+  @Post('teacher')
+  @ApiCreatedResponse({ description: 'The teacher has been successfully created.' })
+  async createTeacher(@Body() createTeacherDto: CreateTeacherDto) {
+    try {
+      await this.userService.createTeacher(createTeacherDto, createTeacherDto.subject);
+
+      return {
+        success: true,
+        message: 'Teacher Created Successfully'
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message
+      };
+    }
+  }
+
+  @ApiCreatedResponse({ description: 'The student has been successfully updated.' })
   @Patch(':id')
   async update(@Param('id', new ParseUUIDPipe()) id: UUID, @Body() updateUserDto: UpdateUserDto) {
     try {
@@ -152,9 +192,44 @@ export class UserController {
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: 'The user has been successfully deleted.' })
   async remove(@Param('id', new ParseUUIDPipe()) id: UUID) {
     try {
       await this.userService.removeUser(id);
+      return {
+        success: true,
+        message: 'User Deleted Successfully'
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message
+      };
+    }
+  }
+
+  @Delete('teacher/:id')
+  @ApiCreatedResponse({ description: 'The teacher has been successfully deleted.' })
+  async removeTeacher(@Param('id', new ParseUUIDPipe()) id: UUID) {
+    try {
+      await this.userService.removeTeacher(id);
+      return {
+        success: true,
+        message: 'User Deleted Successfully'
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message
+      };
+    }
+  }
+
+  @Delete('student/:id')
+  @ApiCreatedResponse({ description: 'The student has been successfully deleted.' })
+  async removeStudent(@Param('id', new ParseUUIDPipe()) id: UUID) {
+    try {
+      await this.userService.removeStudent(id);
       return {
         success: true,
         message: 'User Deleted Successfully'

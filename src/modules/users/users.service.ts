d8b96f47f5b13603fcde07f865ba1e4890c5ad4a
HuyHue <dqh1005@gmail.com>
2024-09-13 17:23:27 +0700

    feat(forgot-password): implement feature forgot-password

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UUID } from 'crypto';
 import { UserNotFoundException } from 'src/exceptions/users/userNotFound.excetion';
@@ -8,6 +8,7 @@ import { User } from '../../entities/user.entity';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserPaginationDto } from './dto/user-pagination.dto';
 import { UsersRepository } from './users.repository';
+import { ERRORS_DICTIONARY } from 'src/constraints/error-dictionary.constraint';
 
 @Injectable()
 export class UsersService {
@@ -62,5 +63,15 @@ export class UsersService {
 
       await this.usersRepository.createUser(newUser);
     }
+  }
+  async updatePassword(newPassword: string, userId:UUID) {
+    await this.findOne(userId)
+    const raw = await this.usersRepository.updatePassword(newPassword, userId);
+    if (raw === 0) {
+      throw new BadRequestException({
+        message:ERRORS_DICTIONARY.RESET_PASSWORD_FAIL
+      })
+    }
+    return raw
   }
 }
